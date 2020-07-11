ff18fb38e007b757029119d966df0ada1c33969b
oclyke <oclyke@gmail.com>
2020-07-11 02:22:55 -0600

    add preliminary networking between hub and column controllers

--- firmware/main/column_controller/column_controller.ino
+++ firmware/main/column_controller/column_controller.ino
@@ -3,10 +3,98 @@
 // file 'LICENSE.md', which is part of this source code package.
 */
 
+#include "src/components/cart/cart.h"
+
+#include "src/components/configuration/configuration.h"
+
+#include "WiFi.h"
+#include "WiFiUdp.h"
+#include "AsyncTCP.h" // https://github.com/me-no-dev/AsyncTCP
+
+#define DEBUG_PORT Serial
+#define DEBUG_BAUD (115200)
+
+volatile bool button0 = false;
+volatile bool wifi_connected = false;
+volatile bool client_connected = false;
+IPAddress IP;
+IPAddress ServerIP;
+WiFiUDP udp;
+AsyncClient client;
+
+void onClientConnected(void* arg, AsyncClient* client){
+  client_connected = true;
+  DEBUG_PORT.println("Client connected to server");
+}
+
+void WiFiEvent(WiFiEvent_t event){
+    switch(event) {
+      case SYSTEM_EVENT_STA_GOT_IP:
+          IP = WiFi.localIP();
+          DEBUG_PORT.print("WiFi connected! IP address: ");
+          DEBUG_PORT.println(IP);  
+          udp.begin(IP, NETWORK_UDP_PORT);
+          wifi_connected = true;
+          break;
+      case SYSTEM_EVENT_STA_DISCONNECTED:
+          DEBUG_PORT.println("WiFi lost connection");
+          wifi_connected = false;
+          break;
+      default: break;
+    }
+}
+
+void IRAM_ATTR button0ISR() {
+  button0 = true;
+}
+
 void setup() {
+  DEBUG_PORT.begin(DEBUG_BAUD);
+
+  pinMode(0, INPUT_PULLUP);
+  attachInterrupt(0, button0ISR, RISING);
+
+  WiFi.disconnect(true);
+  WiFi.onEvent(WiFiEvent);
+  WiFi.begin(NETWORK_SSID, NETWORK_PASSWORD);
+
+  client.onConnect(onClientConnected, NULL);
+  while(!client_connected){
+    if(wifi_connected){
+      if(!udp.parsePacket()){ continue; }
+
+      ServerIP = udp.remoteIP();
+      uint16_t ServerPort = udp.remotePort();
+      uint32_t len = udp.available();
+
+      char payload[len+1];
+      memset(payload, 0x00, len+1);
+      udp.read(payload, len);
+      udp.flush();
   
+      DEBUG_PORT.print("UDP packet rx. len = ");
+      DEBUG_PORT.print(len);
+      DEBUG_PORT.print(", remoteIP = ");
+      DEBUG_PORT.print(ServerIP);
+      DEBUG_PORT.print(", remotePort = ");
+      DEBUG_PORT.print(ServerPort);
+      DEBUG_PORT.print(", payload = '");
+      DEBUG_PORT.print(String(payload));
+      DEBUG_PORT.print("'");
+      DEBUG_PORT.println();
+
+      if((ServerPort == NETWORK_UDP_PORT) && (String(payload) == String(NETWORK_PASSWORD))){
+        client_connected = client.connect(ServerIP, NETWORK_TCP_PORT);
+      }
+    }
+  }
+  udp.stop();
 }
 
 void loop() {
-
+  if(button0){
+    client.close(true);
+    client.stop();
+    button0 = false;
+  }
 }
--- firmware/main/network_hub/network_hub.ino
+++ firmware/main/network_hub/network_hub.ino
@@ -8,6 +8,10 @@
 
 #include "src/components/configuration/configuration.h"
 
+#include "WiFi.h"
+#include "WiFiUdp.h"
+#include "AsyncTCP.h" // https://github.com/me-no-dev/AsyncTCP
+
 #define DEBUG_PORT Serial
 #define DEBUG_BAUD (115200)
 
@@ -16,15 +20,49 @@
 UARTBridge <cart_t> cartBridge(BRIDGE_PORT);
 cart_t cart;
 
+IPAddress IP;
+WiFiUDP udp;
+AsyncServer server(NETWORK_TCP_PORT);
+
 void random_cart( void ){
   cart.col = random(0, STAFF_COLS);
   cart.row = random(0, STAFF_ROWS);
   cart.val = random(0, STAFF_VALS);
 }
 
+void broadcastIP( void * params ){
+  while(1){
+    udp.beginPacket(NETWORK_UDP_ADDRESS, NETWORK_UDP_PORT);
+    udp.print(NETWORK_PASSWORD);
+    udp.endPacket();
+    delay(NETWORK_UDP_BROADCAST_PERIOD);
+  }
+  vTaskDelete( NULL );
+}
+
+void handleClientDisconnected(void* arg, AsyncClient* client){
+  DEBUG_PORT.print("Client 0x");
+  DEBUG_PORT.print((uint32_t)client, HEX);
+  DEBUG_PORT.println(" disconnected!");
+}
+
+void handleClientConnected(void* arg, AsyncClient* client){
+  DEBUG_PORT.print("Client 0x");
+  DEBUG_PORT.print((uint32_t)client, HEX);
+  DEBUG_PORT.println(" connected!");
+
 void setup() {
   DEBUG_PORT.begin(DEBUG_BAUD);
   BRIDGE_PORT.begin(BRIDGE_BAUD);
+
+  WiFi.softAP(NETWORK_SSID, NETWORK_PASSWORD);
+  IP = WiFi.softAPIP();
+
+  server.onClient(handleClientConnected, NULL);
+  server.begin();
+  
+  udp.begin(IP, NETWORK_UDP_PORT);
+  xTaskCreate(broadcastIP, "broadcastIP", 10000, NULL, 1, NULL);
 }
 
 void loop() {
