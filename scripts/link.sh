07f53ac2b0a421e5633ba64651c2c8cbe7c5b8fa
oclyke <owen.lyke@sparkfun.com>
2020-07-15 01:43:03 -0600

    Update link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR+=/..
+
 components="cart cat configuration sensor staff uart_bridge"
 programs="column_controller network_hub conductor"
 
 for program in $programs
 do
+    rm -rf $DIR/firmware/main/$program/src/components
+    mkdir -p $DIR/firmware/main/$program/src/components
     for component in $components
     do
-        rm -rf firmware/main/$program/src/components/$component
-        ln -s firmware/components/$component firmware/main/$program/src/components
+        ln -s $DIR/firmware/components/$component $DIR/firmware/main/$program/src/components/$component
     done
 done
