1050e39765decc8f143f7a3b780c1c10b8644dd3
joeryanxis <37460306+joeryanxis@users.noreply.github.com>
2020-07-26 15:28:02 -0600

    remove erroneous 'main' dir from commands

--- scripts/link.bat
+++ scripts/link.bat
@@ -4,8 +4,8 @@ set components=cart cat configuration sensor staff uart_bridge
 set programs=main\column_controller main\network_hub main\conductor utility\column_checker
 
 for %%G in (%programs%) do (
-    rmdir firmware\main\%%G\src\components /s /q
-    mkdir firmware\main\%%G\src\components
+    rmdir firmware\%%G\src\components /s /q
+    mkdir firmware\%%G\src\components
     for %%H in (%components%) do (
         mklink /J firmware\%%G\src\components\%%H firmware\components\%%H
     )
