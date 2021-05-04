b7dea53c479e4bcc82923b8c61f076943fdaca03
oclyke <owen.lyke@sparkfun.com>
2021-05-03 22:57:48 -0600

    Update index.ts

--- web/rpi/src/index.ts
+++ web/rpi/src/index.ts
@@ -255,6 +255,15 @@ const playColumn = (col: (number | null)[]) => {
     if(i !== null){
       const path = instruments[i].notes[idx].path;
       let player = spawn('omxplayer', ['--no-keys', path, '&']);
+      player.stdout.on('data', (data: any) => {
+        console.log(data)
+      })
+      player.stderr.on('data', (data: any) => {
+        console.warn('error: ', data)
+      })
+      player.on('close', (code: any) => {
+        console.log(`child process exited with code ${code}`);
+      });
     }
   })
 
